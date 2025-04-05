f87c737e72e40782b8f079b71e574c520c2d1b46
Cassandra Cook <cassandracook@gmail.com>
2025-04-05 07:59:51 -0700

    Implement Logging

--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 import { RequestError, ValidationError } from "../http-errors";
+import logger from "../logger";
 
 export type ResponseType = 'api' | 'server';
 
@@ -24,6 +25,8 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = 'server') => {
     if (error instanceof RequestError) {
+        logger.error({ err: error }),
+         `${responseType.toUpperCase()} Error: ${error.message}`
         return formatResponse(responseType, error.statusCode, error.message, error.errors);
     }
 
@@ -32,6 +35,7 @@ const handleError = (error: unknown, responseType: ResponseType = 'server') => {
           error.flatten().fieldErrors as Record<string, string[]>
         );
     
+        logger.error({err: error }, `Validation Error: ${validationError.message}`);
         return formatResponse(
           responseType,
           validationError.statusCode,
@@ -41,13 +45,16 @@ const handleError = (error: unknown, responseType: ResponseType = 'server') => {
       }
 
     if (error instanceof Error) {
+        logger.error(error.message);
+
         return formatResponse(responseType, 500, error.message);
     }
 
+    logger.error({err: error}, "An unexpected error occurred");
     return formatResponse(
         responseType,
         500,
-        "Anunexpected error occurred"
+        "An unexpected error occurred"
     )
 } 
 
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,4 +1,5 @@
 import mongoose, { Mongoose } from 'mongoose';
+import logger from './logger';
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
@@ -23,6 +24,7 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
     if (cached.conn) {
+        logger.info('Using existing mongoose connection');
         return cached.conn;
     }
 
@@ -31,11 +33,11 @@ const dbConnect = async (): Promise<Mongoose> => {
             dbName: "devflow"
         })
         .then((result) => {
-            console.log("Connected to MongoDB");
+            logger.info("Connected to MongoDB");
             return result;
         })
         .catch((error) => {
-            console.log("Error connecting to MongoDB", error);
+            logger.error("Error connecting to MongoDB", error);
             throw error;
         });
         }
